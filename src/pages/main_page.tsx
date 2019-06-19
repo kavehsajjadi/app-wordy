a56fbf17c9205ea9214c078025065d59f7b7738d
Kaveh Sajjadi <kaveh.sajjadi@gmail.com>
2019-06-19 22:38:28 +1000

    fix errors

--- src/pages/main_page.tsx
+++ src/pages/main_page.tsx
@@ -1,9 +1,16 @@
 import * as React from "react"
 import { WiktionaryClient } from "wiktionary_client"
+import { match } from "react-router-dom"
 
 const searchClient = new WiktionaryClient()
 
-export class MainPage extends React.Component {
+type MainProps = {
+  required: string
+  match?: match<{ page?: string; section?: string }>
+  location?: Location
+}
+
+export class MainPage extends React.Component<MainProps> {
   state: {
     searchQuery: string
     searchResults: Array<string>[]
@@ -14,18 +21,48 @@ export class MainPage extends React.Component {
   private readonly onSearchInputChange = (
     e: React.ChangeEvent<HTMLInputElement>,
   ) => {
-    this.setState({ searchQuery: e.target.value })
+    this.setSearchQuery(e.target.value)
+  }
+
+  private readonly setSearchQuery = (searchQuery: string) => {
+    this.setState({ searchQuery })
+  }
+
+  private readonly search = (searchQuery: string): Promise<any> => {
+    const response = searchClient.search(searchQuery)
+    response.then(searchResults => this.setState({ searchResults }))
+    return response
+  }
+
+  private readonly getPage = (page: string): Promise<any> => {
+    const response = searchClient.get(page)
+    response.then(currentPage => this.setState({ currentPage }))
+    return response
   }
 
   private readonly onSearch = () => {
-    const response = searchClient.search(this.state.searchQuery)
-    response.then(searchResults => this.setState({ searchResults }))
+    this.search(this.state.searchQuery)
   }
 
   private readonly onSelectResult = (tag: string, url: string) => {
     this.setState({ currentResult: tag })
-    const response = searchClient.get(tag)
-    response.then(currentPage => this.setState({ currentPage }))
+    this.getPage(tag)
+  }
+
+  componentDidMount() {
+    const { page, section } = this.props.match.params || undefined
+    const params = new URLSearchParams(this.props.location.search)
+    // const section = params.get("section")
+    // const section = params.get('section')
+
+    if (page) {
+      this.setSearchQuery(page)
+      const searchPromise = this.search(page)
+      searchPromise.then(result => {
+        const pageName = result[0][0]
+        this.getPage(pageName)
+      })
+    }
   }
 
   render() {
