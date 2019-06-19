f043a62309843566691e47b0749e1d501fc5ca1a
Kaveh Sajjadi <kaveh.sajjadi@gmail.com>
2019-06-19 22:38:35 +1000

    add new route

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,8 +1,17 @@
 import { MainPage } from "pages/main_page"
+import { TranslatePage } from "pages/translate"
 
 export const routes = [
   {
     component: MainPage,
     path: "/",
+  },
+  {
+    component: MainPage,
+    path: "/main/:page",
+  },
+  {
+    component: TranslatePage,
+    path: "/translate",
   },
 ]
