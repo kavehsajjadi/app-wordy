5472cfd000f17608522cbf90b8292604eb24ab9e
Kaveh Sajjadi <kaveh.sajjadi@gmail.com>
2019-06-30 22:58:16 +1000

    update sidebar story

--- src/ui/sidebar/sidebar.stories.tsx
+++ src/ui/sidebar/sidebar.stories.tsx
@@ -1,13 +1,13 @@
 import React from "react"
 import { storiesOf } from "@storybook/react"
+import { action } from "@storybook/addon-actions"
 import { FullPageContainer } from "storybook/full_page_container"
-import { Sidebar, SidebarView } from "ui/sidebar"
+import { Sidebar } from "./sidebar"
 
-storiesOf("Sidebar", module)
-  .add("example", () => (
-    <FullPageContainer>
-      <SidebarView open={true}>
-        <span>Sidebar view content</span>
-      </SidebarView>
-    </FullPageContainer>
-  ))
+storiesOf("Sidebar", module).add("example", () => (
+  <FullPageContainer>
+    <Sidebar open={true} onToggleClick={action("onToggleClick")}>
+      <span>Sidebar view content</span>
+    </Sidebar>
+  </FullPageContainer>
+))
